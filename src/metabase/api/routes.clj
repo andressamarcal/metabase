4a9563f8ee1d3bfc87ed3f420765eded0dc28d49
Cam Saul <cammsaul@gmail.com>
2018-03-13 15:05:11 -0700

    Fix insanely slow lein ring server

--- src/metabase/api/routes.clj
+++ src/metabase/api/routes.clj
@@ -51,7 +51,7 @@
   "Wrap ROUTES so they may only be accessed with proper apikey credentials."
   middleware/enforce-api-key)
 
-(def +auth
+(def ^:private +auth
   "Wrap ROUTES so they may only be accessed with proper authentiaction credentials."
   middleware/enforce-authentication)
 
--- src/metabase/mt/api/routes.clj
+++ src/metabase/mt/api/routes.clj
@@ -2,27 +2,26 @@
   "Multi-tenant API routes."
   (:require [clojure.tools.logging :as log]
             [compojure.core :refer [defroutes context routes]]
-            [metabase.api.routes :as api-routes]
-            [metabase.mt.api.gtap :as gtap]))
+            [metabase.mt.api.gtap :as gtap]
+            [metabase.middleware :as middleware]))
+
+;; this is copied from `metabase.api.routes` because if we require that above we will destroy startup times for `lein
+;; ring server`
+(def ^:private +auth
+  "Wrap ROUTES so they may only be accessed with proper authentiaction credentials."
+  middleware/enforce-authentication)
 
 (defroutes ^{:doc "Ring routes for mt API endpoints."} mt-routes
   (context
    "/mt"
    []
    (routes
-    (context "/gtap" [] (api-routes/+auth gtap/routes)))))
-
-(def ^:private combined-routes
-  "A combined Compojure routes object that combines the `mt-routes` above with the normal API routes.
-   (This is just the multi-tenant routes list above with the normal routes added afterwards.)"
-  (routes
-   mt-routes
-   api-routes/routes))
-
+    (context "/gtap" [] (+auth gtap/routes)))))
 
 (defn install-mt-routes!
   "Swap out `metabase.api.routes/routes` with a new version that includes the multi-tenant routes. Take care to only
   call this once, or your life may be filled with nastiness!"
   []
   (log/info "Installing multi-tenant API routes...")
-  (intern 'metabase.api.routes 'routes combined-routes))
+  (require 'metabase.api.routes)
+  (intern 'metabase.api.routes 'routes (routes mt-routes @(resolve 'metabase.api.routes/routes))))
