47609841d0ce659b2e83f2c531dcc2b6a1366ac4
Tom Robinson <tlrobinson@gmail.com>
2018-05-29 16:11:07 -0700

    Replace QuestionsLoader with QuestionListLoader

--- frontend/src/metabase/containers/QuestionPicker.jsx
+++ frontend/src/metabase/containers/QuestionPicker.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import CardPicker from "metabase/pulse/components/CardPicker";
-import QuestionsLoader from "metabase/containers/QuestionsLoader";
+import QuestionListLoader from "metabase/containers/QuestionListLoader";
 
 export default class QuestionPicker extends React.Component {
   render() {
     return (
-      <QuestionsLoader>
-        {questions => <CardPicker {...this.props} cardList={questions} />}
-      </QuestionsLoader>
+      <QuestionListLoader>
+        {({ questions }) => <CardPicker {...this.props} cardList={questions} />}
+      </QuestionListLoader>
     );
   }
 }
