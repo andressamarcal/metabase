e24b5b71552eeafc0d4d577f6f8b33116a8bf9f6
Tom Robinson <tlrobinson@gmail.com>
2018-05-30 11:38:50 -0700

    Fix whitelabel settings slug

--- frontend/src/metabase/admin/settings/selectors.js
+++ frontend/src/metabase/admin/settings/selectors.js
@@ -418,6 +418,7 @@ const SECTIONS = [
   },
   {
     name: "Whitelabel",
+    slug: "whitelabel",
     settings: [
       {
         key: "application-name",
@@ -485,6 +486,12 @@ const SECTIONS = [
     }
     */
 ];
+
+for (const section of SECTIONS) {
+  if (section.slug == null) {
+    console.warn("Warning: settings section missing slug:", section.name);
+  }
+}
 
 export const getSettings = createSelector(
   state => state.settings.settings,
