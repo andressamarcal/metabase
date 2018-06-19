e4a62405fb73712584d6824f7eae56328a8c6a63
Tom Robinson <tlrobinson@gmail.com>
2018-06-18 17:14:20 -0700

    Minor i18n string fix

--- frontend/src/metabase/home/components/Activity.jsx
+++ frontend/src/metabase/home/components/Activity.jsx
@@ -251,7 +251,7 @@ export default class Activity extends Component {
         break;
       case "install":
         description.userName = t`Hello World!`;
-        description.summary = t`Metabase` + " is up and running.";
+        description.summary = t`Metabase is up and running.`;
         break;
       case "metric-create":
         if (item.model_exists) {
