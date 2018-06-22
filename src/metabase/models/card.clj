d93998482f0fa4e2599030ebd609a36abb612b22
Ryan Senior <senior.ryan@gmail.com>
2018-06-22 15:40:12 -0500

    Fix segmented permissions for Card PUTs
    If the user changed the query associated with the card and had valid
    segmented permissions, the user would falsely get a 403.
    
    This adds the same check on PUT that is used for POST. It also hids
    the segmented and full table permissions check behind a function so as
    to not sprinkle segmented perms through-out the API layer.

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -98,10 +98,7 @@
     ;; Make sure the User saving the Card has the appropriate permissions to run its query. We don't want Users saving
     ;; Cards with queries they wouldn't be allowed to run!
     (when *current-user-id*
-      (when-not (or (perms/set-has-full-permissions-for-set? @*current-user-permissions-set*
-                      (query-perms/perms-set query :throw-exceptions))
-                    (perms/set-has-full-permissions-for-set? @*current-user-permissions-set*
-                      (query-perms/segmented-perms-set query)))
+      (when-not (query-perms/can-run-query? query)
         (throw (Exception. (str (tru "You do not have permissions to run ad-hoc native queries against Database {0}."
                                      (:database query)))))))
     ;; make sure this Card doesn't have circular source query references
