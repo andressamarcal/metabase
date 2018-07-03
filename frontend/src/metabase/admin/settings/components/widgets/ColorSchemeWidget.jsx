78248a2cbb143daa75b9c85c4c20b21691612fe5
Tom Robinson <tlrobinson@gmail.com>
2018-07-03 15:44:44 -0700

    Clean up whitelabel settings

--- frontend/src/metabase/admin/settings/components/widgets/ColorSchemeWidget.jsx
+++ frontend/src/metabase/admin/settings/components/widgets/ColorSchemeWidget.jsx
@@ -4,9 +4,20 @@ import ColorPicker from "metabase/components/ColorPicker";
 import Button from "metabase/components/Button";
 import Icon from "metabase/components/Icon";
 import _ from "underscore";
-import { capitalize } from "metabase/lib/formatting";
+import { humanize } from "metabase/lib/formatting";
 
 import { originalColors } from "metabase/lib/whitelabel";
+
+const THEMEABLE_COLORS = ["brand"].concat(
+  Object.keys(originalColors).filter(name => name.startsWith("accent")),
+);
+
+const COLOR_DISPLAY_PROPERTIES = {
+  brand: {
+    name: "Brand",
+    description: "Brand is blah blah blah",
+  },
+};
 
 const ColorSchemeWidget = ({ setting, onChange }) => {
   const value = setting.value || {};
@@ -16,45 +27,38 @@ const ColorSchemeWidget = ({ setting, onChange }) => {
     <div>
       <table>
         <tbody>
-          {Object.entries(colors).map(([name, color]) => (
-            <tr>
-              <td>
-                {name
-                  .replace("bg", "background")
-                  .replace(/\d+/, match => " " + match)
-                  .split("-")
-                  .map(capitalize)
-                  .join(" ")}:
-              </td>
-              <td>
-                <span className="mx1">
-                  <ColorPicker
-                    fancy
-                    triggerSize={16}
-                    value={color}
-                    onChange={color => onChange({ ...value, [name]: color })}
-                  />
-                </span>
-                {color !== originalColors[name] && (
-                  <Icon
-                    name="close"
-                    className="text-grey-2 text-grey-4-hover cursor-pointer"
-                    onClick={() => onChange({ ...value, [name]: undefined })}
-                  />
-                )}
-              </td>
-            </tr>
-          ))}
+          {THEMEABLE_COLORS.map(name => {
+            const properties = COLOR_DISPLAY_PROPERTIES[name] || {};
+            return (
+              <tr>
+                <td>{properties.name || humanize(name)}:</td>
+                <td>
+                  <span className="mx1">
+                    <ColorPicker
+                      fancy
+                      triggerSize={16}
+                      value={colors[name]}
+                      onChange={color => onChange({ ...value, [name]: color })}
+                    />
+                  </span>
+                </td>
+                <td>
+                  {colors[name] !== originalColors[name] && (
+                    <Icon
+                      name="close"
+                      className="text-grey-2 text-grey-4-hover cursor-pointer"
+                      onClick={() => onChange({ ...value, [name]: undefined })}
+                    />
+                  )}
+                </td>
+                <td>
+                  <span className="mx2">{properties.description}</span>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-      <div className="pt1">
-        <Button
-          onClick={() => onChange({})}
-          disabled={Object.keys(value).length === 0}
-        >
-          Reset
-        </Button>
-      </div>
     </div>
   );
 };
