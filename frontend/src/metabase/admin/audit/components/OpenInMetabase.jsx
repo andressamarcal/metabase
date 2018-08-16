43717b38178584b34a93a0167f081c00c7f4815e
Maz Ameli <maz@metabase.com>
2018-08-16 11:02:44 -0700

    make open in mb links open in a new tab

--- frontend/src/metabase/admin/audit/components/OpenInMetabase.jsx
+++ frontend/src/metabase/admin/audit/components/OpenInMetabase.jsx
@@ -6,7 +6,7 @@ import Link from "metabase/components/Link";
 import Icon from "metabase/components/Icon";
 
 const OpenInMetabase = ({ ...props }) => (
-  <Link {...props} className="link flex align-center">
+  <Link {...props} className="link flex align-center" target="_blank">
     <Icon name="external" className="mr1" />
     Open in Metabase
   </Link>
