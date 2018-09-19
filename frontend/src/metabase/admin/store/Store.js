292f8551ca077521e1b3a7219a102b6c0067086f
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-09-19 11:09:36 -0700

    icon wrapper

--- frontend/src/metabase/admin/store/Store.js
+++ frontend/src/metabase/admin/store/Store.js
@@ -24,7 +24,7 @@ class StoreApp extends React.Component {
         <Box>
           <StoreDetails />
         </Box>
-        <Card p={3}>
+        <Card p={3} w={"35%"}>
           <h4>{t`Already have a key?`}</h4>
           <Link to="/admin/store/activate">
             <Button>{t`Activate a product`}</Button>
@@ -35,6 +35,20 @@ class StoreApp extends React.Component {
   }
 }
 
+const IconWrapper = ({ children }) => (
+  <Flex
+    align="center"
+    justify="center"
+    p={2}
+    bg={colors["brand"]}
+    color="white"
+    w="40px"
+    style={{ borderRadius: 99, height: 40 }}
+  >
+    {children}
+  </Flex>
+);
+
 class StoreDetails extends React.Component {
   render() {
     return (
@@ -42,8 +56,10 @@ class StoreDetails extends React.Component {
         <h2>{t`Enterprise features`}</h2>
         <Box is="ul" className="text-measure">
           <Flex>
-            <Icon name="lock" size={28} />
-            <Box>
+            <IconWrapper>
+              <Icon name="lock" size={28} />
+            </IconWrapper>
+            <Box ml={1}>
               <h2>{t`Row level permissions`}</h2>
               <Text>
                 {t`Make sure you're showing the right people the right data with automatic and secure filters based on user attributes.`}
@@ -51,8 +67,10 @@ class StoreDetails extends React.Component {
             </Box>
           </Flex>
           <Flex>
-            <Icon name="star" size={28} />
-            <Box>
+            <IconWrapper>
+              <Icon name="star" size={28} />
+            </IconWrapper>
+            <Box ml={1}>
               <h2>{t`Whitelabeling`}</h2>
               <Text>
                 {t`Match Metabase to your brand with custom colors, your own logo and more.`}
@@ -60,8 +78,10 @@ class StoreDetails extends React.Component {
             </Box>
           </Flex>
           <Flex>
-            <Icon name="star" size={28} />
-            <Box>
+            <IconWrapper>
+              <Icon name="star" size={28} />
+            </IconWrapper>
+            <Box ml={1}>
               <h2>{t`SSO`}</h2>
               <Text>
                 {t`Provide easy login that works with your exisiting authentication infrastructure.`}
