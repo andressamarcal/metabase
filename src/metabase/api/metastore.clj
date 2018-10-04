8caa8c9c2b7d4c8c4b620c41e19ef6495a9a9cf8
Cam Saul <cammsaul@gmail.com>
2018-10-04 13:43:51 -0700

    GET /api/metastore/token/:token/status -> GET /api/metastore/token/status

--- src/metabase/api/metastore.clj
+++ src/metabase/api/metastore.clj
@@ -3,10 +3,10 @@
             [metabase.api.common :as api]
             [metabase.public-settings.metastore :as metastore]))
 
-(api/defendpoint GET ["/token/:token/status" :token #"[0-9a-f]{64}"]
-  "Fetch info about MetaStore premium features token include whether it is `valid`, a `trial` token, its `features`, and
-  when it is `valid_thru`."
-  [token]
-  (metastore/fetch-token-status token))
+(api/defendpoint GET "/token/status"
+  "Fetch info about the current MetaStore premium features token including whether it is `valid`, a `trial` token, its
+  `features`, and when it is `valid_thru`."
+  []
+  (metastore/fetch-token-status (api/check-404 (metastore/premium-embedding-token))))
 
 (api/define-routes api/+check-superuser)
