7f114a341e16a1b0afb03758e77cf09e0ff3b0d8
Tom Robinson <tlrobinson@gmail.com>
2019-01-11 10:16:06 -0800

    prettier

--- frontend/src/metabase/visualizations/index.js
+++ frontend/src/metabase/visualizations/index.js
@@ -107,8 +107,11 @@ const extractRemappedColumns = data => {
     row.filter((value, colIndex) => {
       const col = cols[colIndex];
       if (col.remapped_from != null) {
-        if (!cols[col.remapped_from_index] || !cols[col.remapped_from_index].remapping) {
-          console.warn("Invalid remapped_from", col)
+        if (
+          !cols[col.remapped_from_index] ||
+          !cols[col.remapped_from_index].remapping
+        ) {
+          console.warn("Invalid remapped_from", col);
           return true;
         }
         cols[col.remapped_from_index].remapped_to_column = col;
