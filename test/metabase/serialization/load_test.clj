52ddb9a409eacc6bb21268c36200af1f8eb6bd94
Simon Belak <simon@metabase.com>
2019-03-01 20:44:54 +0100

    tests: correctly cleanup

--- test/metabase/serialization/load_test.clj
+++ test/metabase/serialization/load_test.clj
@@ -1,20 +1,27 @@
 (ns metabase.serialization.load-test
-  (:require [expectations :refer :all]
+  (:require [clojure.data :as diff]
+            [expectations :refer :all]
             [metabase.cmd.serialization :refer :all]
             [metabase.models
              [card :refer [Card]]
              [collection :refer [Collection]]
              [dashboard :refer [Dashboard]]
              [dashboard-card :refer [DashboardCard]]
-             [database :refer [Database]]
+             [dashboard-card-series :refer [DashboardCardSeries]]
+             [database :as database :refer [Database]]
+             [dependency :refer [Dependency]]
+             [dimension :refer [Dimension]]
              [field :refer [Field]]
+             [field-values :refer [FieldValues]]
              [metric :refer [Metric]]
+             [pulse :refer [Pulse]]
+             [pulse-card :refer [PulseCard]]
+             [pulse-channel :refer [PulseChannel]]
              [segment :refer [Segment]]
-             [table :refer [Table]]]
-            [metabase.serialization.upsert :as upsert]
+             [table :refer [Table]]
+             [user :refer [User]]]
             [metabase.test.data.users :as test-users]
             [metabase.test.serialization :as ts]
-            [metabase.util :as u]
             [toucan.db :as db]))
 
 (defn- delete-directory
@@ -27,28 +34,47 @@
 
 (def ^:private dump-dir "test-dump")
 
+(defn- world-snapshot
+  []
+  (into {} (for [model [Database Table Field Metric Segment Collection Dashboard DashboardCard
+                        Card DashboardCardSeries FieldValues Dimension Dependency PulseCard PulseChannel]]
+             [model (db/select-field :id model)])))
+
+(defmacro with-world-cleanup
+  [& body]
+  `(let [snapshot# (world-snapshot)]
+     (try
+       ~@body
+       (finally
+         (doseq [[model# ids#] (second (diff/diff snapshot# (world-snapshot)))]
+           (some->> ids#
+                    not-empty
+                    (vector :in)
+                    (db/delete! model# :id)))))))
+
 (expect
   (try
     (let [fingerprint (ts/with-world
                         (dump dump-dir (:email (test-users/fetch-user :crowberto)))
-                        [[Database db-id]
-                         [Table table-id]
-                         [Field numeric-field-id]
-                         [Field category-field-id]
-                         [Collection collection-id]
-                         [Collection collection-id-nested]
-                         [Metric metric-id]
-                         [Segment segment-id]
-                         [Dashboard dashboard-id]
-                         [Card card-id]
-                         [Card card-id-root]
-                         [Card card-id-nested]
-                         [DashboardCard dashcard-id]])]
-      (load dump-dir {:on-error :abort})
-      (u/prog1 (every? (fn [[model id]]
-                         (#'upsert/select-identical model (model id)))
-                       fingerprint)
-        (doseq [[model id] fingerprint]
-          (db/delete! model :id id))))
+                        [[Database (Database db-id)]
+                         [Table (Table table-id)]
+                         [Field (Field numeric-field-id)]
+                         [Field (Field category-field-id)]
+                         [Collection (Collection collection-id)]
+                         [Collection (Collection collection-id-nested)]
+                         [Metric (Metric metric-id)]
+                         [Segment (Metric segment-id)]
+                         [Dashboard (Dashboard dashboard-id)]
+                         [Card (Card card-id)]
+                         [Card (Card card-id-root)]
+                         [Card (Card card-id-nested)]
+                         [DashboardCard (DashboardCard dashcard-id)]])]
+      (with-world-cleanup
+        (load dump-dir {:on-error :abort :mode :skip})
+        (every? (fn [[model entity]]
+                        (and entity
+                             (or (-> entity :name nil?)
+                                 (db/select model :name (:name entity)))))
+                      fingerprint)))
     (finally
       (delete-directory dump-dir))))
