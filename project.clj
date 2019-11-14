c44fc324bf9c07f71875d63135ef9df75507a59e
Cam Saul <cammsaul@gmail.com>
2019-11-13 22:28:56 -0800

    Fix loading Oracle driver/linters

--- project.clj
+++ project.clj
@@ -251,7 +251,10 @@
 
    :include-all-drivers
    [:with-include-drivers-middleware
-    {:include-drivers :all}]
+    {:include-drivers :all
+     :injections
+     [(require 'metabase.plugins)
+      (metabase.plugins/load-plugins!)]}]
 
    :repl
    [:include-all-drivers
