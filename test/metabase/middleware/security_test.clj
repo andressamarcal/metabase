f5565ed0a909352f31641d01e632e8ed6b8c1ed9
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2019-11-22 21:44:39 +0200

    Add tests for CSP header `frame-ancestors` directive

---
+++ test/metabase/middleware/security_test.clj
@@ -0,0 +1,28 @@
+(ns metabase.middleware.security-test
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
+            [metabase.middleware.security :as mw.security]
+            [metabase.public-settings :as public-settings]
+            [metabase.test.util :as tu]))
+
+(defn- csp-frame-ancestors-directive
+  []
+  (-> (mw.security/security-headers)
+      (get "Content-Security-Policy")
+      (str/split #"; *")
+      (as-> xs (filter #(str/starts-with? % "frame-ancestors ") xs))
+      first))
+
+(deftest csp-header-frame-ancestor-tests
+  (testing "Frame ancestors from `embedding-app-origin` setting"
+    (let [multiple-ancestors "https://*.metabase.com http://metabase.internal"]
+      (tu/with-temporary-setting-values [enable-embedding     true
+                                         embedding-app-origin multiple-ancestors]
+        (is (= (str "frame-ancestors " multiple-ancestors)
+               (csp-frame-ancestors-directive))))))
+
+  (testing "Frame ancestors is 'none' for nil `embedding-app-origin`"
+    (tu/with-temporary-setting-values [enable-embedding     true
+                                       embedding-app-origin nil]
+      (is (= "frame-ancestors 'none'"
+             (csp-frame-ancestors-directive))))))
