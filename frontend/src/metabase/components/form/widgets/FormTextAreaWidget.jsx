5a40d4fdbf8d7ef7d357b6b0005b8d48bb208f8c
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2019-12-16 23:27:30 +0200

    Linter appeasement

--- frontend/src/metabase/components/form/widgets/FormTextAreaWidget.jsx
+++ frontend/src/metabase/components/form/widgets/FormTextAreaWidget.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import cx from "classnames";
 
 const FormTextAreaWidget = ({ placeholder, field, monospaceText }) => (
-  <textarea className={cx("Form-input full", { "text-monospace": monospaceText })} placeholder={placeholder} {...field} />
+  <textarea
+    className={cx("Form-input full", { "text-monospace": monospaceText })}
+    placeholder={placeholder}
+    {...field}
+  />
 );
 
 export default FormTextAreaWidget;
