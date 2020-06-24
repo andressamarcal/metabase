2809778a492d6d07ccb5b4d909aa0412d159f48c
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-06-24 12:22:02 -0700

    prettier

--- enterprise/frontend/test/metabase-enterprise/_support_/cypress.js
+++ enterprise/frontend/test/metabase-enterprise/_support_/cypress.js
@@ -1,95 +1,94 @@
 import {
-    signIn,
-    openProductsTable,
-    signOut,
-  } from "../../../../../frontend/test/__support__/cypress";
-  
-  const today = new Date().getDate().toString();
-  export const date = " " + today + ",";
-  
-  // Change settings like theme colors, sandbox permissions, etc.
-  
-  export function changeThemeColor(location, colorhex) {
-    cy.get("td")
-      .eq(location)
-      .click();
-    cy.get(`div[title='#${colorhex}']`).click();
-    cy.findByText("Done").click();
+  signIn,
+  openProductsTable,
+  signOut,
+} from "../../../../../frontend/test/__support__/cypress";
+
+const today = new Date().getDate().toString();
+export const date = " " + today + ",";
+
+// Change settings like theme colors, sandbox permissions, etc.
+
+export function changeThemeColor(location, colorhex) {
+  cy.get("td")
+    .eq(location)
+    .click();
+  cy.get(`div[title='#${colorhex}']`).click();
+  cy.findByText("Done").click();
+}
+export function changePermissionsforSandbox(
+  location,
+  column,
+  user_attribute,
+  first,
+) {
+  cy.findByText("Data permissions");
+  cy.get(".ReactVirtualized__Grid__innerScrollContainer")
+    .children()
+    .eq(location)
+    .click();
+  cy.findByText("Grant sandboxed access").click();
+  if (first == "first") {
+    cy.findByText("Change").click();
   }
-  export function changePermissionsforSandbox(
-    location,
-    column,
-    user_attribute,
-    first,
-  ) {
-    cy.findByText("Data permissions");
-    cy.get(".ReactVirtualized__Grid__innerScrollContainer")
-      .children()
-      .eq(location)
-      .click();
-    cy.findByText("Grant sandboxed access").click();
-    if (first == "first") {
-      cy.findByText("Change").click();
-    }
-    cy.get(".Icon-chevrondown")
+  cy.get(".Icon-chevrondown")
+    .first()
+    .click();
+  cy.findByText(column).click();
+  cy.get(".Icon-chevrondown")
+    .last()
+    .click();
+  cy.findAllByText(user_attribute)
+    .last()
+    .click();
+  cy.findByText("Save").click();
+}
+
+// Generate information like questions, dashboards, etc.
+
+export function generateQuestions(users) {
+  users.forEach(user => {
+    signIn(user);
+
+    openProductsTable();
+    cy.wait(2000)
+      .findAllByText("Summarize")
       .first()
+      .click({ force: true });
+    cy.wait(2000)
+      .get(".List-section")
+      .eq(users.indexOf(user) + 1)
       .click();
-    cy.findByText(column).click();
-    cy.get(".Icon-chevrondown")
-      .last()
-      .click();
-    cy.findAllByText(user_attribute)
-      .last()
-      .click();
+    cy.findByText("Done").click();
+
     cy.findByText("Save").click();
-  }
-  
-  // Generate information like questions, dashboards, etc.
-  
-  export function generateQuestions(users) {
-    users.forEach(user => {
-      signIn(user);
-  
-      openProductsTable();
-      cy.wait(2000)
-        .findAllByText("Summarize")
-        .first()
-        .click({ force: true });
-      cy.wait(2000)
-        .get(".List-section")
-        .eq(users.indexOf(user) + 1)
-        .click();
-      cy.findByText("Done").click();
-  
-      cy.findByText("Save").click();
-      cy.findByLabelText("Name")
-        .clear()
-        .type(user + " test q");
-      cy.get(".Icon-chevrondown").click();
-      cy.wait(500)
-        .findAllByText("Our analytics")
-        .last()
-        .click();
-      cy.findAllByText("Save")
-        .last()
-        .click();
-      cy.findByText("Not now").click();
-  
-      signOut();
-    });
-  }
-  export function generateDashboards(users) {
-    users.forEach(user => {
-      signIn(user);
-      cy.visit("/");
-      cy.get(".Icon-add").click();
-      cy.findByText("New dashboard").click();
-      cy.findByLabelText("Name").type(user + " test dash");
-      cy.get(".Icon-chevrondown").click();
-      cy.findAllByText("Our analytics")
-        .last()
-        .click();
-      cy.findByText("Create").click();
-    });
-  }
-  
+    cy.findByLabelText("Name")
+      .clear()
+      .type(user + " test q");
+    cy.get(".Icon-chevrondown").click();
+    cy.wait(500)
+      .findAllByText("Our analytics")
+      .last()
+      .click();
+    cy.findAllByText("Save")
+      .last()
+      .click();
+    cy.findByText("Not now").click();
+
+    signOut();
+  });
+}
+export function generateDashboards(users) {
+  users.forEach(user => {
+    signIn(user);
+    cy.visit("/");
+    cy.get(".Icon-add").click();
+    cy.findByText("New dashboard").click();
+    cy.findByLabelText("Name").type(user + " test dash");
+    cy.get(".Icon-chevrondown").click();
+    cy.findAllByText("Our analytics")
+      .last()
+      .click();
+    cy.findByText("Create").click();
+  });
+}
--- enterprise/frontend/test/metabase-enterprise/formatting/sandboxes.cy.spec.js
+++ enterprise/frontend/test/metabase-enterprise/formatting/sandboxes.cy.spec.js
@@ -1,177 +1,178 @@
 import {
-    restore,
-    signInAsAdmin,
-    openOrdersTable,
-    signInAsNormalUser,
-    signOut,
-  } from "../../../../../frontend/test/__support__/cypress";
-  
-  import { changePermissionsforSandbox } from "../_support_/cypress";
-  
-  const new_user = {
-    first_name: "Barb",
-    last_name: "Tabley",
-    username: "new@metabase.com",
-  };
-  
-  let questionId;
-  
-  describe("formatting > sandboxes", () => {
-    before(restore);
-  
-    describe("Setup for sandbox tests", () => {
-      beforeEach(signInAsAdmin);
-  
-      it("should make SQL question", () => {
-        cy.request("POST", "/api/card", {
-          name: "sql param",
-          dataset_query: {
-            type: "native",
-            native: {
-              query:
-                "select * from products join orders on products.id = orders.product_id where orders.created_at > dateadd(year,-5,getdate())",
-            },
-            database: 1,
+  restore,
+  signInAsAdmin,
+  openOrdersTable,
+  signInAsNormalUser,
+  signOut,
+} from "../../../../../frontend/test/__support__/cypress";
+
+import { changePermissionsforSandbox } from "../_support_/cypress";
+
+const new_user = {
+  first_name: "Barb",
+  last_name: "Tabley",
+  username: "new@metabase.com",
+};
+
+let questionId;
+
+describe("formatting > sandboxes", () => {
+  before(restore);
+
+  describe("Setup for sandbox tests", () => {
+    beforeEach(signInAsAdmin);
+
+    it("should make SQL question", () => {
+      cy.request("POST", "/api/card", {
+        name: "sql param",
+        dataset_query: {
+          type: "native",
+          native: {
+            query:
+              "select * from products join orders on products.id = orders.product_id where orders.created_at > dateadd(year,-5,getdate())",
           },
-          display: "scalar",
-          visualization_settings: {},
-        }).then(({ body }) => {
-          questionId = body.id;
-        });
-  
-        cy.visit("/collection/root");
-        cy.wait(2000)
-          .findByText("sql param")
-          .click();
-        cy.findByText("Save").click();
-        cy.findAllByText("Save")
-          .last()
-          .click();
+          database: 1,
+        },
+        display: "scalar",
+        visualization_settings: {},
+      }).then(({ body }) => {
+        questionId = body.id;
       });
-  
-      it("should make a JOINs table", () => {
-        openOrdersTable();
-        cy.wait(1000)
-          .get(".Icon-notebook")
-          .click();
-        cy.wait(1000)
-          .findByText("Join data")
-          .click();
-        cy.findByText("Products").click();
-        cy.findByText("Visualize").click();
-        cy.findByText("Save").click();
-  
-        cy.findByLabelText("Name")
-          .clear()
-          .wait(1)
-          .type("test joins table");
-        cy.findAllByText("Save")
-          .last()
-          .click();
-        cy.findByText("Not now").click();
-      });
+
+      cy.visit("/collection/root");
+      cy.wait(2000)
+        .findByText("sql param")
+        .click();
+      cy.findByText("Save").click();
+      cy.findAllByText("Save")
+        .last()
+        .click();
     });
-  
-    describe("Sandboxes should work", () => {
-      beforeEach(signInAsNormalUser);
-  
-      it("should add key attributes to new user and existing user", () => {
-        signOut();
-        signInAsAdmin();
-  
-        // Existing user
-        cy.visit("/admin/people");
-        cy.get(".Icon-ellipsis")
-          .last()
-          .click();
-        cy.findByText("Edit user").click();
-        cy.findByText("Add an attribute").click();
-        cy.findByPlaceholderText("Key").type("User ID");
-        cy.findByPlaceholderText("Value").type("3");
-        cy.findByText("Update").click();
-  
-        // New user
-        cy.visit("/admin/people");
-        cy.findByText("Add someone").click();
-        cy.findByPlaceholderText("Johnny").type(new_user.first_name);
-        cy.findByPlaceholderText("Appleseed").type(new_user.last_name);
-        cy.findByPlaceholderText("youlooknicetoday@email.com").type(
-          new_user.username,
-        );
-        cy.findByText("Add an attribute").click();
-        cy.findByPlaceholderText("Key").type("User ID");
-        cy.findByPlaceholderText("Value").type("1");
-        cy.findAllByText("Create").click();
-        cy.findByText("Done").click();
-      });
-  
-      it("should change sandbox permissions as admin", () => {
-        signOut();
-        signInAsAdmin();
-  
-        cy.visit("/admin/permissions/databases/1/schemas/PUBLIC/tables");
-        changePermissionsforSandbox(13, "User ID", "User ID", "first");
-        changePermissionsforSandbox(18, "ID", "User ID", "second");
-        cy.findByText("Save Changes").click();
-        cy.findByText("Yes").click();
-        cy.findByText("Save Changes").should("not.exist");
-      });
-  
-      it("should filter on normal tables", () => {
-        cy.visit("/browse/1");
-        cy.findByText("Orders").click();
-  
-        // Table filter
-        cy.wait(3000)
-          .get(".TableInteractive-cellWrapper--lastColumn")
-          .contains("1")
-          .should("not.exist");
-        cy.get(".TableInteractive-cellWrapper--lastColumn")
-          .last()
-          .contains("3");
-  
-        // Notebook filter
-        cy.get(".Icon-notebook").click();
-        cy.wait(2000)
-          .findByText("Summarize")
-          .click();
-        cy.findByText("Count of rows").click();
-        cy.findByText("Visualize").click();
-        cy.findByText("18,760").should("not.exist");
-        cy.findByText("10");
-      });
-  
-      it("should filter on saved SQL question", () => {
-        cy.visit("/question/4");
-        cy.get(".TableInteractive-cellWrapper--firstColumn")
-          .should("have.length", 11)
-      });
-  
-      it("should filter on a JOINed table", () => {
-        cy.visit("/question/5");
-  
-        // Table filter
-        cy.wait(2000)
-          .get(".TableInteractive-cellWrapper--firstColumn")
-          .should("have.length", 11);
-  
-        // Notebook filter
-        cy.get(".Icon-notebook").click();
-        cy.wait(2000)
-          .findByText("Filter")
-          .click();
-        cy.findAllByText("Total")
-          .last()
-          .click();
-        cy.findByText("Equal to").click();
-        cy.findByText("Greater than").click();
-        cy.findByPlaceholderText("Enter a number").type("100");
-        cy.findByText("Add filter").click();
-        cy.findByText("Visualize").click();
-        cy.wait(2000)
-          .get(".TableInteractive-cellWrapper--firstColumn")
-          .should("have.length", 7);
-      });
+
+    it("should make a JOINs table", () => {
+      openOrdersTable();
+      cy.wait(1000)
+        .get(".Icon-notebook")
+        .click();
+      cy.wait(1000)
+        .findByText("Join data")
+        .click();
+      cy.findByText("Products").click();
+      cy.findByText("Visualize").click();
+      cy.findByText("Save").click();
+
+      cy.findByLabelText("Name")
+        .clear()
+        .wait(1)
+        .type("test joins table");
+      cy.findAllByText("Save")
+        .last()
+        .click();
+      cy.findByText("Not now").click();
     });
   });
-  
+
+  describe("Sandboxes should work", () => {
+    beforeEach(signInAsNormalUser);
+
+    it("should add key attributes to new user and existing user", () => {
+      signOut();
+      signInAsAdmin();
+
+      // Existing user
+      cy.visit("/admin/people");
+      cy.get(".Icon-ellipsis")
+        .last()
+        .click();
+      cy.findByText("Edit user").click();
+      cy.findByText("Add an attribute").click();
+      cy.findByPlaceholderText("Key").type("User ID");
+      cy.findByPlaceholderText("Value").type("3");
+      cy.findByText("Update").click();
+
+      // New user
+      cy.visit("/admin/people");
+      cy.findByText("Add someone").click();
+      cy.findByPlaceholderText("Johnny").type(new_user.first_name);
+      cy.findByPlaceholderText("Appleseed").type(new_user.last_name);
+      cy.findByPlaceholderText("youlooknicetoday@email.com").type(
+        new_user.username,
+      );
+      cy.findByText("Add an attribute").click();
+      cy.findByPlaceholderText("Key").type("User ID");
+      cy.findByPlaceholderText("Value").type("1");
+      cy.findAllByText("Create").click();
+      cy.findByText("Done").click();
+    });
+
+    it("should change sandbox permissions as admin", () => {
+      signOut();
+      signInAsAdmin();
+
+      cy.visit("/admin/permissions/databases/1/schemas/PUBLIC/tables");
+      changePermissionsforSandbox(13, "User ID", "User ID", "first");
+      changePermissionsforSandbox(18, "ID", "User ID", "second");
+      cy.findByText("Save Changes").click();
+      cy.findByText("Yes").click();
+      cy.findByText("Save Changes").should("not.exist");
+    });
+
+    it("should filter on normal tables", () => {
+      cy.visit("/browse/1");
+      cy.findByText("Orders").click();
+
+      // Table filter
+      cy.wait(3000)
+        .get(".TableInteractive-cellWrapper--lastColumn")
+        .contains("1")
+        .should("not.exist");
+      cy.get(".TableInteractive-cellWrapper--lastColumn")
+        .last()
+        .contains("3");
+
+      // Notebook filter
+      cy.get(".Icon-notebook").click();
+      cy.wait(2000)
+        .findByText("Summarize")
+        .click();
+      cy.findByText("Count of rows").click();
+      cy.findByText("Visualize").click();
+      cy.findByText("18,760").should("not.exist");
+      cy.findByText("10");
+    });
+
+    it("should filter on saved SQL question", () => {
+      cy.visit("/question/4");
+      cy.get(".TableInteractive-cellWrapper--firstColumn").should(
+        "have.length",
+        11,
+      );
+    });
+
+    it("should filter on a JOINed table", () => {
+      cy.visit("/question/5");
+
+      // Table filter
+      cy.wait(2000)
+        .get(".TableInteractive-cellWrapper--firstColumn")
+        .should("have.length", 11);
+
+      // Notebook filter
+      cy.get(".Icon-notebook").click();
+      cy.wait(2000)
+        .findByText("Filter")
+        .click();
+      cy.findAllByText("Total")
+        .last()
+        .click();
+      cy.findByText("Equal to").click();
+      cy.findByText("Greater than").click();
+      cy.findByPlaceholderText("Enter a number").type("100");
+      cy.findByText("Add filter").click();
+      cy.findByText("Visualize").click();
+      cy.wait(2000)
+        .get(".TableInteractive-cellWrapper--firstColumn")
+        .should("have.length", 7);
+    });
+  });
+});
