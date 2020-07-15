ec3d09e2dfd17903192da873484b2be5704e5de8
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2020-07-15 09:43:49 -0400

    fix picker root collection name

--- frontend/src/metabase/components/form/widgets/FormSnippetCollectionWidget.jsx
+++ frontend/src/metabase/components/form/widgets/FormSnippetCollectionWidget.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-import CollectionSelect from "metabase/containers/CollectionSelect";
+import ItemSelect from "metabase/containers/ItemSelect";
+import CollectionPicker from "metabase/containers/CollectionPicker";
 import SnippetCollections from "metabase/entities/snippet-collections";
 
-const FormSnippetCollectionWidget = ({ field }) => (
-  <CollectionSelect entity={SnippetCollections} {...field} />
+const CollectionSelect = ItemSelect(
+  CollectionPicker,
+  SnippetCollections.Name,
+  "collection",
 );
 
+const FormSnippetCollectionWidget = ({ field }) => (
+  <CollectionSelect
+    entity={SnippetCollections}
+    {...field}
+    value={field.value || "root"} // needed so SnippetCollections.Name finds the right collection
+  />
+);
 export default FormSnippetCollectionWidget;
